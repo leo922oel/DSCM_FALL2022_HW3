db31f14e54dcac6852f91f36a5c66fcd7cfd61a2
leo922oel <leo922oel@gmail.com>
2022-12-27 15:30:18 +0800

    update wrong pi estimation

--- q3.py
+++ q3.py
@@ -16,8 +16,8 @@ def pi_estimation(n, h):
     
     circle = X[:, t[0,:]==1]
     square = X[:, t[0,:]==0]
-    plt.scatter(circle[0,:], circle[1,:], s=0.5, color="blue")
-    plt.scatter(square[0,:], square[1,:], s=0.5, color="green")
+    # plt.scatter(circle[0,:], circle[1,:], s=0.5, color="blue")
+    # plt.scatter(square[0,:], square[1,:], s=0.5, color="green")
     
     return t.sum() / n * 4
 
@@ -30,19 +30,35 @@ def wrong_pi_estimation(n, h):
     while t != n:
         X[0, t] = X[0, t-1] + 2 * uniform(0, h) - 1
         X[1, t] = X[1, t-1] + 2 * uniform(0, h) - 1 
-        total += 1
+        if (X[0, t] <= 1 
+            and X[0, t] >= -1
+            and X[1, t] <= 1
+            and X[1, t] >= -1
+            ):
+            total += 1
 
-        if X[0, t]**2 + X[1, t]**2 <= 1:
-            t += 1
-        else:
-            outside.append((X[0, t], X[1, t]))
+            if X[0, t]**2 + X[1, t]**2 <= 1:
+                t += 1
+            else:
+                outside.append((X[0, t], X[1, t]))
     
     outside = np.array(outside)
-    print(outside)
-    plt.scatter(X[0,:], X[1,:], s=0.5, color="blue")
-    plt.scatter(outside[:, 0], outside[:, 1], s=0.7, color="green")
+    # plt.scatter(X[0,:], X[1,:], s=0.5, color="blue")
+    # plt.scatter(outside[:, 0], outside[:, 1], s=0.7, color="green")
     
-    return t / total * 4
+    return t / total * 4, total
+
 #%%
-print(pi_estimation(20000, 1))
+# print(pi_estimation(20000, 1))
 # print(wrong_pi_estimation(20000, 1))
+
+pi_list = np.zeros(1000)
+for i in range(1000):
+    if (i+1) % 100 == 0: print(i)
+    pi_list[i] = pi_estimation(20000, 1)
+#%%
+plt.title("Simulation with 1000 epochs")
+plt.hist(pi_list)
+plt.text(3.16, 170, f"mean: {round(np.mean(pi_list), 3)}\nstd: {round(np.std(pi_list), 3)}", fontsize=12)
+
+# %%
